69a9283abac630305f7e44f417f7156cfbc48d9c
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2021-01-05 12:00:45 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
+  
     @brand = brand
+    BRANDS << brand
+
+    puts BRANDS.uniq!
+
+    puts "end"
+
   end
 
   def cobble
@@ -11,4 +20,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+
 end
+
+shoe_1 = Shoe.new("Nike")
+p shoe_1
+
